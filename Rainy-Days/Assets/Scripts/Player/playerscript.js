57804b2dbc832d2b46c362a41f170c9f6ae6c246
rmccampbell <rmccampbell7@gmail.com>
2015-02-22 18:06:45 -0500

    Added a gitignore, hopefully will make less git problems, might make more and removed the unnecessary files (Library/*, etc.)

--- Rainy-Days/Assets/Scripts/Player/playerscript.js
+++ Rainy-Days/Assets/Scripts/Player/playerscript.js
@@ -1,22 +1,38 @@
 ﻿#pragma strict
 
 static var speed : int = 10;
-
+static var jumpspeed : int = 20;
+var grounded : boolean = false;
 
 function Start () {
+}
 
+function OnCollisionEnter2D(coll: Collision2D) {
+	print("Coll Enter: " + coll.contacts[0].normal);
+	if (coll.contacts[0].normal.x < coll.contacts[0].normal.y) {
+    	grounded = true;
+//    	print("Ground collision");
+	}
+}
+
+function OnCollisionExit2D(coll: Collision2D) {
+    	print("Coll Exit: " + coll.contacts[0].normal);
+    	grounded = false;
+//    	print("Ground collision exit");
 }
 
 function Update () {
 
-  if (Input.GetKey (KeyCode.UpArrow)) {
-         rigidbody2D.velocity = Vector2(0, 5);
-         }
-         if (Input.GetKey (KeyCode.LeftArrow)) {
-         transform.Translate (Vector2(-1,0) * Time.deltaTime*speed);
-         }
-         if (Input.GetKey (KeyCode.RightArrow)) {
-         transform.Translate (Vector2(1,0) * Time.deltaTime*speed);
-         }
+	if (Input.GetKey(KeyCode.UpArrow) && grounded) {
+		rigidbody2D.velocity.y = jumpspeed;
+	}
+	if (Input.GetKey(KeyCode.LeftArrow)) {
+//		rigidbody2D.velocity.x = -speed;
+		transform.Translate (Vector2(-1,0) * Time.deltaTime*speed);
+	}
+	if (Input.GetKey(KeyCode.RightArrow)) {
+//		rigidbody2D.velocity.x = speed;
+		transform.Translate (Vector2(1,0) * Time.deltaTime*speed);
+	}
 
 }
