b9eee59fa690b784497b53f3ba0b12df520b907e
EMILY <ERC7AS@VIRGINIA.EDU>
2015-04-28 21:05:27 -0400

    monster fix and moving platforms
    nimboids no longer affected by lag
    added moving platforms

--- Rainy-Days/Assets/Scripts/Player/playerscript.js
+++ Rainy-Days/Assets/Scripts/Player/playerscript.js
@@ -62,6 +62,9 @@ function OnCollisionEnter2D(coll: Collision2D) {
 function OnCollisionStay2D(coll: Collision2D) {
 	if (coll.contacts[0].normal.y > 0) {
     	grounded = true;
+    	if (coll.gameObject.name == "moving platform") {
+			transform.Translate(Vector2(coll.gameObject.transform.position.x-transform.position.x,0) * Time.deltaTime * 6);
+		}
 	}
 }
 
